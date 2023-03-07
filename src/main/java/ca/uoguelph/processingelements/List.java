f6b88f5650f5109e0d51a620dbe0a8dd876aa07f
akganapat <116813652+akganapat@users.noreply.github.com>
2023-03-07 12:30:18 -0500

    Made list proc element

--- src/main/java/ca/uoguelph/processingelements/List.java
+++ src/main/java/ca/uoguelph/processingelements/List.java
@@ -4,13 +4,37 @@ import ca.uoguelph.storageelements.StorageElement;
 import java.util.ArrayList;
 
 public class List implements ProcessingElement {
-    public List(long max) {
 
+    private long maxFolderEntries;
+
+    public List(long max) {
+        this.maxFolderEntries = max;
     }
 
     @Override
     public ArrayList<StorageElement> process(ArrayList<StorageElement> input) {
-        return input;
+        ArrayList<StorageElement> output = new ArrayList<>();
+
+        for (int i = 0; i < input.size(); i++) {
+            if (input.get(i).isDirectory()) {
+                ArrayList<StorageElement> childElements = input.get(i).getChildStorageElements();
+
+                if (maxFolderEntries < childElements.size()) {
+                    for (int j = 0; j < maxFolderEntries; j++) {
+                        output.add(childElements.get(j));
+                    }
+                } else {
+                    for (int j = 0; j < childElements.size(); j++) {
+                        output.add(childElements.get(j));
+                    }
+                }
+
+            } else {
+                output.add(input.get(i)); //if its a file pass it through element
+
+            }
+        }
+        return output;
     }
 
     @Override
