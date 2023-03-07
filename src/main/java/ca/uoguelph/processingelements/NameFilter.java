74f3f6573514642689ef734c6da0fbf99e04bac2
akganapat <116813652+akganapat@users.noreply.github.com>
2023-03-07 11:23:40 -0500

    Implement name filter

--- src/main/java/ca/uoguelph/processingelements/NameFilter.java
+++ src/main/java/ca/uoguelph/processingelements/NameFilter.java
@@ -4,13 +4,20 @@ import ca.uoguelph.storageelements.StorageElement;
 import java.util.ArrayList;
 
 public class NameFilter implements ProcessingElement {
+    private String searchKey;
     public NameFilter(String searchKey) {
-
+        this.searchKey = searchKey;
     }
 
     @Override
     public ArrayList<StorageElement> process(ArrayList<StorageElement> input) {
-        return input;
+        ArrayList<StorageElement> output = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++){
+            if (input.get(i).name().contains(this.searchKey)) {
+                output.add(input.get(i));
+            }
+        }
+        return output;
     }
 
     @Override
