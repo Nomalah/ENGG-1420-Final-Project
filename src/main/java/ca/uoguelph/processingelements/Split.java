a63e4b90dce610d55f3669ac9d74dfe91a2ac749
agihekler <74014368+agihekler@users.noreply.github.com>
2023-03-13 12:22:35 -0400

    partially implemented split processing element

--- src/main/java/ca/uoguelph/processingelements/Split.java
+++ src/main/java/ca/uoguelph/processingelements/Split.java
@@ -2,15 +2,16 @@ package ca.uoguelph.processingelements;
 
 import ca.uoguelph.storageelements.StorageElement;
 import java.util.ArrayList;
-import java.io.*;
-import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Split implements ProcessingElement {
 
-    int lines;
-    int length; // do I need variable for this or does it come from length filter?
+    int target_lines;
 
-    public Split(int lines) {
+    public Split(int target_lines) {
 
     }
 
@@ -18,7 +19,6 @@ public class Split implements ProcessingElement {
     // function
     public ArrayList<StorageElement> process(ArrayList<StorageElement> input) {
         ArrayList<StorageElement> output = new ArrayList<>();
-        int count = 0;
         // iterate through output ArrayList
         for (int i = 0; i < input.size(); i++) {
             boolean bool;
@@ -29,56 +29,80 @@ public class Split implements ProcessingElement {
                 return input;
                 // else is file
             } else if (bool == false) {
-                // create a new array list with each element as new line 
-                List<String> linesList = new ArrayList<>();
-                BufferedReader br = null;
-                count++;
+                String fileContents = input.get(i).read();
+                String[] fileLines = fileContents.split("\n");
+                String nameOfEntry = input.get(i).name();
 
-                try {
-                    // do we need to keep original file names?
-                    br = new BufferedReader(new FileReader("file" + count + ".txt"));
-                    String line;
-
-                    // read file line by line for input # of lines
-                    while ((line = br.readLine()) != null) {
-                        // add each line to array list
-                        linesList.add(line);
-
+                // count number of Lines in file
+                int numOfLines = 0;
+                for (int j = 0; j < fileLines.length; j++) {
+                    if (fileLines[j] != null) {
+                        numOfLines++;
                     }
-                    int check = lines % length;
-                    int numofFiles, fileLines, remLines;
-                    File f;
-                    if (check == 0) {
-                        numofFiles = check;
-                        fileLines = lines;
+                }
+                String newName;
+                int new_files;
+                int num_arrays = 0;
 
-                        // create number of required files
-                        for (int j = 0; j < numofFiles; j++) {
-                            // naming convention for new files
-                            f = new File("file" + i + "_copy.txt");
-                            
-                            // add specified lines from array list to each file here!
-                        }
+                int rest = fileLines.length % target_lines;
+                int parts = fileLines.length / target_lines + (rest > 0 ? 1 : 0);
+                String[][] arrays = new String[parts][];
+                for (int j = 0; j < (rest > 0 ? parts - 1 : parts); j++) {
+                    arrays[i] = Arrays.copyOfRange(fileLines, j * target_lines, j * target_lines + target_lines);
+                    num_arrays++;
 
-                    } else {
-                        numofFiles = check + 1;
-                        for (int j = 0; j < check + 1; j++) {
-                            fileLines = lines;
-                        }
-                        remLines = check;
-                        // create number of required files
-                        for (int k = 0; k < check + 1; check++) {
-                            f = new File("file" + i + "_copy.txt");
-                             // add specified lines from array list to each file here!
-                        }
-
-                    }
-                } catch (IOException e) {
-                    System.out.println("An error occurred.");
-                    e.printStackTrace();
+                }
+                if (rest > 0) {
+                    arrays[parts - 1] = Arrays.copyOfRange(fileLines, (parts - 1) * target_lines, (parts - 1) * target_lines + rest);
+                    num_arrays++;
                 }
 
+                for (int f = 0; f <= num_arrays; f++) {
+                    newName = nameOfEntry + ".part" + f + 1;
+                    File newFile = new File(newName);
+                    try {
+                        FileWriter writer = new FileWriter(newFile, true);
+                    } catch (IOException ioe) {
+                    }
+
+                }
+
+//                // if file perfectly divisible by target lines
+//                if (numOfLines % target_lines == 0) {
+//                    new_files = numOfLines / target_lines;
+//                    // should I add this inside of rename method in storageElement?
+//                    for (int f = 0; f <= new_files; f++) {
+//                        newName = nameOfEntry + ".part" + f;
+//                        File newFile = new File(newName);
+//                        try {
+//                            FileWriter writer = new FileWriter(newFile, true);
+//                        } catch (IOException ioe) {
+//                        }
+//                        int elecount =0;
+//                        String[] newPart;
+//                        for (int part = 0; part < fileLines.length; target_lines++) {
+//                            newPart[elecount] = part;
+//                            
+//                            
+//                            
+//                            for(int ele=0; ele<newPart.length; ele++){
+//                                
+//                            }
+//
+//                        }
+//
+//                    }
+//
+//                } else {
+//                    new_files = numOfLines / target_lines + 1;
+//                    for (int f = 0; f <= new_files; f++) {
+//                        newName = nameOfEntry + ".part" + f;
+//
+//                    }
+//
+//                }
             }
+
         }
 
         return output;
