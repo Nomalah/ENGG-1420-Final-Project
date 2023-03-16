4aee9bb95ebbf0de6e8fe54f54509c9cad6e47a5
welshowa <116571239+welshowa@users.noreply.github.com>
2023-03-16 15:27:47 -0400

    Fixing some shit

--- src/main/java/ca/uoguelph/processingelements/LengthFilter.java
+++ src/main/java/ca/uoguelph/processingelements/LengthFilter.java
@@ -19,41 +19,43 @@ public class LengthFilter implements ProcessingElement {
         for (int i = 0; i < input.size(); i++) {
             StorageElement element = input.get(i);
             if (element.isFile()) {
-                output.add(element);
-            }
             switch (operator) {
 
             
             case "EQ":
-                    if (element.getSize() == Target_length){
+                    if (element.getLength() == Target_length){
                  output.add(entry);
             }
                 break;
             
             case "NEQ":
-                    if (element.getSize() != Target_length) {
+                    if (element.getLength() != Target_length) {
                 output.add(entry);
             }
                 break;
             
             case "GT":
-                    if (element.getSize() < Target_length) {
+                    if (element.getLength() < Target_length) {
                 output.add(entry);
+            }
                 break;
            
             case "GTE":
-                    if (element.getSize() <= Target_length) {
+                    if (element.getLength() <= Target_length) {
                 output.add(entry);
+            }
                 break;
                     
             case "LT":
-                    if (element.getSize() > Target_length) {
+                    if (element.getLength() > Target_length) {
                 output.add(entry);
+            }
                  break;
                        
             case "LTE":
-                    if (element.getSize() >= Target_length) {
+                    if (element.getLength() >= Target_length) {
                 output.add(entry);
+            }
                 break;
 
             default:
@@ -64,13 +66,9 @@ public class LengthFilter implements ProcessingElement {
     return output;
                 }
 
-                @Override
-                public void print
-                    
-                
                 
                 
                     () {
         System.out.println("LengthFilter");
                 }
-            }
+            
