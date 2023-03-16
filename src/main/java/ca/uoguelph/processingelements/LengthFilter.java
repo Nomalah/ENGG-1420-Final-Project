d88a25b9f42ffb3345950e20b23575ed9d8ea40f
welshowa <116571239+welshowa@users.noreply.github.com>
2023-03-16 15:21:40 -0400

    Length Filter Again

--- src/main/java/ca/uoguelph/processingelements/LengthFilter.java
+++ src/main/java/ca/uoguelph/processingelements/LengthFilter.java
@@ -18,44 +18,47 @@ public class LengthFilter implements ProcessingElement {
         ArrayList<StorageElement> output = new ArrayList<>();
         for (int i = 0; i < input.size(); i++) {
             StorageElement element = input.get(i);
-            if (element.isDirectory()) {
+            if (element.isFile()) {
                 output.add(element);
             }
             switch (operator) {
 
             
-            Case "EQ":
-                    if (element.getLength() == operator){
+            case "EQ":
+                    if (element.getSize() == Target_length){
                  output.add(entry);
             }
-                    break;
-            Case "NEQ":
-                    if (element.getLength() != operator) {
+                break;
+            
+            case "NEQ":
+                    if (element.getSize() != Target_length) {
                 output.add(entry);
             }
-            break;
-            Case "GT":
-                    if (element.getLength() < operator) {
+                break;
+            
+            case "GT":
+                    if (element.getSize() < Target_length) {
                 output.add(entry);
                 break;
-                Case "GTE":
-                    if (element.getLength() <= operator) {
-                    output.add(entry);
-                    break;
-                    Case "LT":
-                    if (element.getLength() > operator) {
-                        output.add(entry);
-                        break;
-                        Case "LTE":
-                    if (element.getLength() >= operator) {
-                            output.add(entry);
-                            break;
-
-                        
+           
+            case "GTE":
+                    if (element.getSize() <= Target_length) {
+                output.add(entry);
+                break;
                     
-                    default:
-                    System.out.println("The operator does not have a value");
-                    break;
+            case "LT":
+                    if (element.getSize() > Target_length) {
+                output.add(entry);
+                 break;
+                       
+            case "LTE":
+                    if (element.getSize() >= Target_length) {
+                output.add(entry);
+                break;
+
+            default:
+            System.out.println("The operator does not have a value");
+            break;
             }
         }
     return output;
