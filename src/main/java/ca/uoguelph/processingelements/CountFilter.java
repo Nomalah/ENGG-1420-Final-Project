8194cd2410f5ba466ba02e9907a01029362183e1
welshowa <116571239+welshowa@users.noreply.github.com>
2023-03-16 15:09:35 -0400

    Working on countfilter

--- src/main/java/ca/uoguelph/processingelements/CountFilter.java
+++ src/main/java/ca/uoguelph/processingelements/CountFilter.java
@@ -4,13 +4,27 @@ import ca.uoguelph.storageelements.StorageElement;
 import java.util.ArrayList;
 
 public class CountFilter implements ProcessingElement {
+
+    private final int minCount;
+    private final String searchKey;
+
     public CountFilter(String searchKey, int minCount) {
 
+        this.searchKey = searchKey;
+        this.minCount = minCount;
+        minCount = Math.max(minCount, 0);
     }
 
     @Override
     public ArrayList<StorageElement> process(ArrayList<StorageElement> input) {
-        return input;
+        ArrayList<StorageElement> countOutput = new ArrayList<>();
+        for (StorageElement element : input) {
+            if (element.isFile() && countOccurances(element.getName(), searchKey) >= minCount)  {
+                countOutput.add(element);
+            }
+
+        }
+        return countOutput;
     }
 
     @Override
