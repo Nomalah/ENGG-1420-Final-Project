23642bdad269e59b90f1924bf613c42e4be5c704
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-03-28 12:54:10 -0400

    Clean up length filter

--- src/main/java/ca/uoguelph/processingelements/LengthFilter.java
+++ src/main/java/ca/uoguelph/processingelements/LengthFilter.java
@@ -4,68 +4,60 @@ import ca.uoguelph.storageelements.StorageElement;
 import java.util.ArrayList;
 
 public class LengthFilter implements ProcessingElement {
-
-    private final long Target_length;
+    private final long targetLength;
     private final String operator;
 
     public LengthFilter(long Target_length, String operator) {
-        this.Target_length = Target_length;
+        this.targetLength = Target_length;
         this.operator = operator;
     }
 
     @Override
     public ArrayList<StorageElement> process(ArrayList<StorageElement> input) {
         ArrayList<StorageElement> output = new ArrayList<>();
-        for (int i = 0; i < input.size(); i++) {
-            StorageElement element = input.get(i);
-            {
-                switch (operator) {
-
-                    case "EQ":
-                        if (element.length() == Target_length) {
-                            output.add(element);
-                        }
-                        break;
-
-                    case "NEQ":
-                        if (element.length() != Target_length) {
-                            output.add(element);
-                        }
-                        break;
-
-                    case "GT":
-                        if (element.length() < Target_length) {
-                            output.add(element);
-                        }
-                        break;
-
-                    case "GTE":
-                        if (element.length() <= Target_length) {
-                            output.add(element);
-                        }
-                        break;
-
-                    case "LT":
-                        if (element.length() > Target_length) {
-                            output.add(element);
-                        }
-                        break;
-
-                    case "LTE":
-                        if (element.length() >= Target_length) {
-                            output.add(element);
-                        }
-                        break;
-
-                    default:
-                        System.out.println("The operator does not have a value");
-                        break;
-                }
+        for (StorageElement element : input) {
+            switch (operator) {
+                case "EQ":
+                    if (element.length() == targetLength) {
+                        output.add(element);
+                    }
+                    break;
+                case "NEQ":
+                    if (element.length() != targetLength) {
+                        output.add(element);
+                    }
+                    break;
+                case "GT":
+                    if (element.length() < targetLength) {
+                        output.add(element);
+                    }
+                    break;
+                case "GTE":
+                    if (element.length() <= targetLength) {
+                        output.add(element);
+                    }
+                    break;
+                case "LT":
+                    if (element.length() > targetLength) {
+                        output.add(element);
+                    }
+                    break;
+                case "LTE":
+                    if (element.length() >= targetLength) {
+                        output.add(element);
+                    }
+                    break;
+                default:
+                    System.out.println("The operator is invalid!");
+                    break;
             }
+        }
 
-            return output;
-        }
-        void print 
-            () { 
-            System.out.println("LengthFilter");
-        }
+        return output;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("LengthFilter");
+    }
+}
