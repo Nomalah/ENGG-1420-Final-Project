e6cdd97cc71d38a806ad086289762fe6ab685282
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-03-28 13:29:01 -0400

    Added rename, clean up code a little bit.

--- src/main/java/ca/uoguelph/processingelements/Print.java
+++ src/main/java/ca/uoguelph/processingelements/Print.java
@@ -4,12 +4,13 @@ import ca.uoguelph.storageelements.StorageElement;
 import java.util.ArrayList;
 
 public class Print implements ProcessingElement {
+
     @Override
     public ArrayList<StorageElement> process(ArrayList<StorageElement> input) {
-        for(int i = 0; i<input.size(); i++){
-            input.get(i).print();
+        for (StorageElement element : input) {
+            element.print(); // Print every element
         }
-        return input;
+        return input; // This processing element does not modify the input
     }
 
     @Override
